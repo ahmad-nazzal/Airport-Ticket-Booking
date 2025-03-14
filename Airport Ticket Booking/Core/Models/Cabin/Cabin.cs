de7a12f08f01399f0b0967aa9fc6dbde20fd347a
Ahmad <nazzala8@gmail.com>
2025-03-14 15:59:07 +0200

    Added core models for Flight, Passenger, Cabin and Booking

---
+++ Airport Ticket Booking/Core/Models/Cabin/Cabin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Core.Models.Cabin
+{
+    public class Cabin
+    {
+        public required Guid Id { get; init; } = Guid.NewGuid();
+        public required CabinClass CabinName { get; init; }
+        public required decimal Price { get; set; }
+        public required int TotalSeats { get; init; }
+        public int AvailableSeats { get; private set; }
+
+    }
+}
---
+++ Airport Ticket Booking/Core/Models/Flight/Flight.cs
@@ -0,0 +1,23 @@
+﻿using Airport_Ticket_Booking.Core.Models.Cabin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Core.Models.Flight
+{
+    public class Flight
+    {
+        public required Guid Id { get; init; } = Guid.NewGuid();
+        public required string FlightName { get; init; }
+        public required string DepartureCountry{ get; init; }
+        public string? DestinationCountry{ get; init; }
+        public string? DepartureAirport{ get; init; }
+        public string? ArrivalAirport{ get; init; }
+        public DateTime DepartureDate { get; init; }
+        public DateTime ArrivalDate { get; init; }
+        public List<Cabin.Cabin> Cabins { get; set; } = new();
+
+    }
+}
