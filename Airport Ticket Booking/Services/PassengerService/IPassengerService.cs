fae548d5ee3cafbe4394847abee85aee63dbb532
Ahmad <nazzala8@gmail.com>
2025-03-16 16:30:39 +0200

    Implement Passenger Services

---
+++ Airport Ticket Booking/Services/PassengerService/IPassengerService.cs
@@ -0,0 +1,15 @@
+﻿using Airport_Ticket_Booking.Models.Passenger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Services.PassengerService
+{
+    public interface IPassengerService
+    {
+        public Passenger GetPassengerById(Guid passengerId);
+        public List<Passenger> GetAllPassengers();
+    }
+}
---
+++ Airport Ticket Booking/Services/PassengerService/PassengerService.cs
@@ -0,0 +1,32 @@
+﻿using Airport_Ticket_Booking.Infrastructure.Repositories.PassengerRepository;
+using Airport_Ticket_Booking.Models.Passenger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Services.PassengerService
+{
+    public class PassengerService: IPassengerService
+    {
+        private readonly IPassengerRepository _passengerRepository;
+        public PassengerService(IPassengerRepository passengerRepository)
+        {
+            _passengerRepository = passengerRepository;
+        }
+        public Passenger GetPassengerById(Guid passengerId)
+        {
+            var passenger = _passengerRepository.GetPassengerById(passengerId);
+            if (passenger == null)
+            {
+                Console.WriteLine("Passenger Not Found");
+            }
+            return passenger;
+        }
+        public List<Passenger> GetAllPassengers()
+        {
+            return _passengerRepository.GetAllPassengers();
+        }
+    }
+}
