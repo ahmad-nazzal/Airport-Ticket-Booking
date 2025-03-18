555f84493d90682797da288fffa918eae77cb8fa
Ahmad <nazzala8@gmail.com>
2025-03-18 02:20:30 +0200

    add FlightFilter record

---
+++ Airport Ticket Booking/Records/FlightFilter.cs
@@ -0,0 +1,21 @@
+﻿using Airport_Ticket_Booking.Models.Cabin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking.Records
+{
+    public record FlightFilter
+    {
+        public decimal? MinPrice = null;
+        public decimal? MaxPrice = null;
+        public string? DepartureCountry = null;
+        public string? DestinationCountry = null;
+        public DateTime? DepartureDate = null;
+        public string? DepartureAirport = null;
+        public string? ArrivalAirport = null;
+        public CabinClass? CabinClass = null;
+    }
+}
